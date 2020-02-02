62926cd24c6a1dc33f6aa0c2629ec79ae3c15975
Marcus Turewicz <24448509+marcusturewicz@users.noreply.github.com>
2020-02-02 11:10:06 +1100

    Enables loading of more tools after first search (#15)

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import axios from 'axios'
 import Tool from '../tool/tool'
-import { FormControl, InputLabel, Input, LinearProgress } from '@material-ui/core';
+import { FormControl, InputLabel, Input, LinearProgress, TablePagination, Button, Grid, CircularProgress } from '@material-ui/core';
 
 export default class Search extends React.Component {
 
     constructor() {
         super()
         this.state = {
+            tools: [],
+            loading: false,
             hasSearched: false,
-            hasData: false,
-            tools: undefined,
-            loading: false
+            hasMoreResults: false,
+            page: 0,
+            pageSize: 20,
+            search: '',
+            newSearch: true,
         }
     }
 
     render() {
         const handleKeyPress = (event) => {
             if (event.key === 'Enter') {
-                this.setState({ loading: true });
-                const q = event.target.value;
-                axios.get(`https://azuresearch-usnc.nuget.org/query?q=${q}&packageType=DotnetTool&skip=0&take=20`).then(response => {
-                    this.setState({ hasSearched: true })
-                    if (response.data.data.length > 0)
-                        this.setState({ hasData: true })
-                    this.setState({ tools: response.data.data });
-                    this.setState({ loading: false });
-                });
+                this.setState({ page: 0, search: event.target.value, newSearch: true }, () => search());
             }
+        }
+        const search = () => {
+            this.setState({ loading: true }, () => {
+                const pageSkip = this.state.page * this.state.pageSize;
+                axios.get(`https://azuresearch-usnc.nuget.org/query?q=${this.state.search}&packageType=DotnetTool&skip=${pageSkip}&take=${this.state.pageSize}`).then(response => {
+                    this.setState({
+                        tools: this.state.newSearch ? response.data.data : this.state.tools.concat(response.data.data),
+                        loading: false,
+                        hasSearched: true,
+                        hasMoreResults: response.data.totalHits > response.data.data.length
+                    });
+                });
+            });
+        }
+        const onLoadMoreClicked = () => {
+            this.setState({ page: this.state.page + 1, newSearch: false }, () => search());
         }
         return (
             <div>
@@ -39,12 +51,23 @@ export default class Search extends React.Component {
                     />
                 </FormControl>
                 {this.state.loading && <LinearProgress />}
-                {this.state.tools && this.state.tools.length > 0 &&
+                {this.state.tools.length > 0 &&
                     <div>
                         <br />
                         {this.state.tools.map(data => <div><Tool key={data.id} value={data}></Tool><br /></div>)}
                     </div>}
-                {this.state.tools && this.state.tools.length === 0 && <p hidden={!this.state.tools}>0 results could be found.</p>}
+                <p hidden={!(this.state.hasSearched && this.state.tools.length === 0)}>No tools could be found.</p>
+                {
+                    this.state.tools.length > 0 &&
+                    <div >
+                        <Grid container justify="center">
+                            {!this.state.loading && <Button disabled={!this.state.hasMoreResults} variant="contained" color="primary" onClick={onLoadMoreClicked}>Load more...</Button>}
+                            {this.state.loading && <CircularProgress />}
+                        </Grid>
+                        <p hidden={this.state.hasMoreResults}>No more results available.</p>                        
+                        <br />
+                    </div>
+                }
             </div>
         )
     }
